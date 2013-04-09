7e725923f54af8a37b473b04bb4f4d5777777dd9
Mahrud Sayrafi <mahrud@eve>
2013-04-09 04:04:20 -0700

    On going ...

--- code.py
+++ code.py
@@ -1,32 +1,32 @@
 import web
-import MySQLdb
+#import MySQLdb
 
-conn = MySQLdb.connection (host='127.0.0.1', user='hellicode', passwd='', db='hellicode')
+#conn = MySQLdb.connection (host='127.0.0.1', user='hellicode', passwd='', db='hellicode')
 
 render = web.template.render('templates/', base='layout')
 urls = (
-  '/',                              'home'
+  '/',                              'home',
 
-#  '/hellicode',                    'dash'
+#  '/hellicode',                    'dash',
 
-#  '/hellicode/problemset/(.*)',    'pset'
-#  '/hellicode/problem/(.*)',       'prob'
-#  '/hellicode/contest/(.*)',       'contest'
+#  '/hellicode/problemset/(.*)',    'pset',
+#  '/hellicode/problem/(.*)',       'prob',
+#  '/hellicode/contest/(.*)',       'contest',
 
-  '/hellicode/submit',              'submit'
+  '/hellicode/submit',              'submit',
 
-#  '/hellicode/ranking',            'rank'
-#  '/hellicode/scoreboard/(.*)',    'board'
+#  '/hellicode/ranking',            'rank',
+#  '/hellicode/scoreboard/(.*)',    'board',
   '/hellicode/status/(.*)',         'stat'
 
-#  '/hellicode/admin',              'admin'
-#  '/hellicode/login',              'login'
-#  '/hellicode/user/(.*)',          'user'
-#  '/hellicode/users',              'users'
-#  '/hellicode/logout',             'logout'
-#  '/hellicode/register',           'register'
+#  '/hellicode/admin',              'admin',
+#  '/hellicode/login',              'login',
+#  '/hellicode/user/(.*)',          'user',
+#  '/hellicode/users',              'users',
+#  '/hellicode/logout',             'logout',
+#  '/hellicode/register',           'register',
 
-#  '/hellicode/news/(.*)',          'news'
+#  '/hellicode/news/(.*)',          'news',
 )
 
 class home:
@@ -35,16 +35,16 @@ class home:
 
 class submit:
     def GET(self):
-        return "hellow"
+        return render.submit()
 
 class stat:
-    def GET(self, content):
-        conn.query("SELECT id, username, problem_id, lang, status, time, memory, date FROM `submitions` where `id`=%s" % (content))
+    def GET(self, subid):
+        conn.query("SELECT * FROM submitions where id=%i" % (subid))
         res = conn.store_result()
-        return render.home(res.fetch_row())
+        return render.stat(res.fetch_row()[0])
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
     app.run()
 
-conn.close()
+#conn.close()
